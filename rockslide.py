75300e98f7cc618404505d382687607cc4ee4873
Nick Farnham <brythonick@gmail.com>
2016-12-16 23:24:20 +0000

    Change gravity to be an optional argument
    If a value is not given as an arg then 9.81 is used as a default. This may be better as the conservative 10 in future.

--- rockslide.py
+++ rockslide.py
@@ -13,11 +13,12 @@ from math import sin, cos, tan, radians
 # SCRIPT ARGUMENTS
 # ============================================================================ #
 parser = ArgumentParser()
+parser.add_argument("-g", "--gravity", help="set the value of gravity",
+                    type=float)
 parser.add_argument("-b", "--bolt", help="include a rock bolt, normal to slope",
                     type=float)
+# TODO: mutually exclusive arg to find required bolt force for given FOS
 required = parser.add_argument_group('required arguments')
-required.add_argument("-g", "--gravity", help="set the value of gravity",
-                      type=float, required=True)
 required.add_argument("-a", "--angle", help="sliding surface angle",
                       type=float, required=True)
 required.add_argument("-f", "--friction", help="rock angle of friction",
@@ -28,15 +29,18 @@ required.add_argument("-m", "--mass", help="mass of the rock block",
                       type=float, required=True)
 args = parser.parse_args()
 
-rock_mass = args.mass
-gravity = args.gravity
-slope_angle = args.angle
-friction_angle = args.friction
-contact_area = args.area
 if not args.bolt:
     rock_bolt = 0
 else:
     rock_bolt = args.bolt
+if not args.gravity:
+    gravity = 9.81
+else:
+    gravity = args.gravity
+rock_mass = args.mass
+slope_angle = args.angle
+friction_angle = args.friction
+contact_area = args.area
 
 
 # ============================================================================ #
@@ -88,3 +92,4 @@ if __name__ == "__main__":
         )
     )
     print("{:0.2f}".format(fos_value))
+    # TODO: add option for more verbose output, with individual calc results
